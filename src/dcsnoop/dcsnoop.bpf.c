707c0c4dcfda9c201288236d243ce36b90432580
wubijie <wubijie@kylinos.cn>
2023-07-01 13:45:54 +0800

    Track directory entries in the Linux kernel

--- src/dcsnoop/dcsnoop.bpf.c
+++ src/dcsnoop/dcsnoop.bpf.c
@@ -92,3 +92,34 @@ static __always_inline int kretprobe__d_lookup(void *ctx, struct dentry *ret)
 	submit_buf(ctx, event, sizeof(*event));
 	return 0;
 }
+
+static __always_inline int
+fexit__d_lookup(void *ctx, const struct dentry *parent,
+		const struct qstr *name, struct dentry *ret)
+{
+	u64 pid_tgid = bpf_get_current_pid_tgid();
+	u32 pid = pid_tgid >> 32;
+	u32 tid = pid_tgid;
+	struct event *event;
+
+	if (ret != NULL)	/* lookup didn't fail */
+		return 0;
+
+	if (target_pid && target_pid != pid)
+		return 0;
+	if (target_tid && target_tid != tid)
+		return 0;
+
+	event = reserve_buf(sizeof(*event));
+	if (!event)
+		return 0;
+
+	event->pid = pid;
+	event->tid = tid;
+	event->type = LOOKUP_MISS;
+	bpf_get_current_comm(&event->comm, sizeof(event->comm));
+	bpf_core_read_str(&event->filename, sizeof(event->filename), name->name);
+
+	submit_buf(ctx, event, sizeof(*event));
+	return 0;
+}
