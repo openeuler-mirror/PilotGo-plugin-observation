b4a926a22ecb218c422abf05c266a0dd4a6b2bc0
wubijie <wubijie@kylinos.cn>
2023-07-03 16:06:12 +0800

    Initialize the execsnoop bpf file constant

---
+++ src/execsnoop/execsnoop.bpf.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "execsnoop.h"
+#include "maps.bpf.h"
+
+#define MAX_ENTRIES	10240
+
+const volatile bool filter_memcg = false;
+const volatile bool ignore_failed = true;
+const volatile uid_t target_uid = INVALID_UID;
+const volatile int max_args = DEFAULT_MAX_ARGS;
+
+static const struct event zero;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);
+	__type(key, u32);
+	__type(value, u32);
+	__uint(max_entries, 1);
+} cgroup_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, pid_t);
+	__type(value, struct event);
+} execs SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u32));
+} events SEC(".maps");
