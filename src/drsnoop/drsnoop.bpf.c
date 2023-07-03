9220eb0baf64f4d4578b97b495c4dcb53c3a0f3d
wubijie <wubijie@kylinos.cn>
2023-07-03 10:20:41 +0800

    track the beginning of direct memory reclaim in a kernel-mode driver

--- src/drsnoop/drsnoop.bpf.c
+++ src/drsnoop/drsnoop.bpf.c
@@ -27,3 +27,25 @@ struct {
 	__uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
+static int handle_direct_reclaim_begin()
+{
+	u64 *vm_zone_stat_kaddrp = (u64 *)vm_zone_stat_kaddr;
+	u64 id = bpf_get_current_pid_tgid();
+	struct piddata piddata = {};
+	u32 tgid = id >> 32;
+	u32 pid = id;
+
+	if (target_tgid && target_tgid != tgid)
+		return 0;
+	if (target_pid && target_pid != pid)
+		return 0;
+
+	piddata.ts = bpf_ktime_get_ns();
+	if (vm_zone_stat_kaddrp) {
+		bpf_core_read(&piddata.nr_free_pages, sizeof(u64),
+			      &vm_zone_stat_kaddrp[NR_FREE_PAGES]);
+	}
+
+	bpf_map_update_elem(&start, &pid, &piddata, BPF_ANY);
+	return 0;
+}
