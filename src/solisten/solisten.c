1796559d07dc1a63e21e6efd28494a7008950d1a
li-long315 <lilong@kylinos.cn>
2023-07-04 08:38:48 +0800

    Parse command line parameters and set the print function of the libbpf library

--- src/solisten/solisten.c
+++ src/solisten/solisten.c
@@ -9,7 +9,27 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-mp", 't', NULL, 0, "Include timestamp on output" },
+static volatile sig_atomic_t exiting;
+
+static pid_t target_pid = 0;
+static bool emit_timestamp = false;
+static bool verbose = false;
+
+const char *argp_program_version = "solisten 0.1";
+const char *argp_program_bug_address = "Jackie Liu <liuyun01@kylinos.cn>";
+const char argp_program_doc[] =
+"Trace IPv4 and IPv6 listen syscalls.\n"
+"\n"
+"USAGE: solisten [-h] [-t] [-p PID]\n"
+"\n"
+"EXAMPLES:\n"
+"    solisten           # trace listen syscalls\n"
+"    solisten -t        # output with timestamp\n"
+"    solisten -p 1216   # only trace PID 1216\n";
+
+static const struct argp_option opts[] = {
+	{ "pid", 'p', "PID", 0, "Process ID to trace" },
+	{ "timestamp", 't', NULL, 0, "Include timestamp on output" },
 	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
 	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
 	{},
@@ -36,6 +56,14 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 	return 0;
 }
 
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
+			   va_list args)
+{
+	if (level == LIBBPF_DEBUG && !verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
 int t argc, char *argv[])
 {
 	LIBBPF_OPTS(bpf_objmain(inect_open_opts, open_opts);
@@ -51,6 +79,11 @@ int t argc, char *argv[])
 	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
 	if (err)
 		return err;
+	
+	if (!bpf_is_root())
+		return 1;
+
+	libbpf_set_print(libbpf_print_fn);
 
 }
 
