af7ffaf371554d068f0f6569d36723cad4049e19
jxy_git <jiangxinyu@kylinos.cn>
2023-07-04 15:00:58 +0800

    Provides version information for the program, contact information for bug reports, and definitions and descriptions of command-line parameters

--- src/funccount/funccount.c
+++ src/funccount/funccount.c
@@ -27,3 +27,28 @@ static struct env {
 	.interval = 99999999,
 	.interations = 9999999,
 };
+
+const char *argp_program_version = "funccount 0.1";
+const char *argp_program_bug_address = "Jackie Liu <liuyun01@kylinos.cn>";
+const char argp_program_doc[] =
+"Count functions, tracepoints.\n"
+"\n"
+"USAGE: funccount [-v] [-i INTERVAL] [-p PID] [-d DURATION] [-T] funcname\n"
+"\n"
+" funccount   func          -- probe a kernel function\n"
+"             lib:func      -- probe a user-space function in the library 'lib\n"
+"             /path:func    -- probe a user-space function in binary '/path'\n"
+"             p::func       -- same thing as 'func'\n"
+"             p:lib:func    -- same thing as 'lib:func'\n"
+"             t:cat:event   -- probe a kernel tracepoint\n"
+"             u:lib:probe   -- probe a USDT tracepoint\n";
+
+static const struct argp_option opts[] = {
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ "interval", 'i', "INTERVAL", 0, "Output interval, in seconds" },
+	{ "pid", 'p', "PID", 0, "Trace process id PID only" },
+	{ "duration", 'd', "DURATION", 0, "total duration of trace, seconds" },
+	{ "timestamp", 'T', NULL, 0, "include timestamp on output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{}
+};
