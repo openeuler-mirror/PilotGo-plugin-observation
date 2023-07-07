4ad033613f2e3dee9843ef4cfe896b0ccb2dff44
jxy_git <jiangxinyu@kylinos.cn>
2023-07-07 15:30:46 +0800

    Realized the function of tracking and information recording of function call entry point

--- src/funcslower/funcslower.bpf.c
+++ src/funcslower/funcslower.bpf.c
@@ -22,3 +22,39 @@ struct entry_t {
 	__u64 start_ns;
 	__u64 args[6];
 };
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1024);
+	__type(key, __u64);
+	__type(value, struct entry_t);
+} entryinfo SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_STACK_TRACE);
+	__type(key, __u32);
+} stack_trace SEC(".maps");
+
+static __always_inline int trace_entry(struct pt_regs *ctx, int id)
+{
+	__u64 pid_tgid = bpf_get_current_pid_tgid();
+	pid_t pid = pid_tgid >> 32;
+
+	if (target_pid && target_pid != pid)
+		return 0;
+
+	struct entry_t entry = {};
+	entry.start_ns = bpf_ktime_get_ns();
+	entry.id = id;
+
+	if (need_grab_args) {
+		entry.args[0] = PT_REGS_PARM1(ctx);
+		entry.args[1] = PT_REGS_PARM2(ctx);
+		entry.args[2] = PT_REGS_PARM3(ctx);
+		entry.args[3] = PT_REGS_PARM4(ctx);
+		entry.args[4] = PT_REGS_PARM5(ctx);
+	}
+
+	bpf_map_update_elem(&entryinfo, &pid_tgid, &entry, BPF_ANY);
+	return 0;
+}
