5649105a15f5a75c130f0a312ea936f3aac59c82
li-long315 <lilong@kylinos.cn>
2023-07-10 17:30:47 +0800

    Add handle_swap_readpage function handles the exchange page read event

--- src/swapin/swapin.bpf.c
+++ src/swapin/swapin.bpf.c
@@ -17,6 +17,27 @@ struct {
 	__type(value, u64);
 } counts SEC(".maps");
 
+static __always_inline int handle_swap_readpage(void)
+{
+	u64 *valp, zero = 0;
+	pid_t pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (target_pid && target_pid != pid)
+		return 0;
+
+	struct key_t key = {
+		.pid = pid
+	};
+	bpf_get_current_comm(&key.comm, sizeof(key.comm));
+	valp = bpf_map_lookup_or_try_init(&counts, &key, &zero);
+	if (!valp)
+		return 0;
+
+	__sync_fetch_and_add(valp, 1);
+
+	return 0;
+}
+
 SEC("kprobe/swap_readpage")
 int BPF_KPROBE(swap_readpage_kprobe)
 {
