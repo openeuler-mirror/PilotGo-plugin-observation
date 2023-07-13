0c9e77c93eb7c86ec0afe77af2be212943532938
li-long315 <lilong@kylinos.cn>
2023-07-13 17:48:32 +0800

    Add a sports map to store the local port numbers to be filtered.

---
+++ src/tcpstates/tcpstates.bpf.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+#include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include "tcpstates.h"
+#include "compat.bpf.h"
+
+#define MAX_ENTRIES	10240
+#define AF_INET		2
+#define AF_INET6	10
+
+const volatile bool filter_by_sport = false;
+const volatile bool filter_by_dport = false;
+const volatile short target_family = 0;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, __u16);
+	__type(value, __u16);
+} sports SEC(".maps");
