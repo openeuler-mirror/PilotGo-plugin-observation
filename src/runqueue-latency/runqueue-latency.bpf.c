6937e0eb56c61d01daae3345e980d53032f924c0
zhanghan2021 <zhanghan@kylinos.cn>
2023-07-13 17:17:38 +0800

    add switch handle func for  runqueue module

--- src/runqueue-latency/runqueue-latency.bpf.c
+++ src/runqueue-latency/runqueue-latency.bpf.c
@@ -82,3 +82,54 @@ static unsigned int pid_namespace(struct task_struct *task)
 
 	return inum;
 }
+
+static int handle_switch(bool preempt, struct task_struct *prev, struct task_struct *next)
+{
+	struct hist *histp;
+	u64 *tsp, slot;
+	u32 pid, hkey;
+	s64 delta;
+
+	if (filter_memcg_fn())
+		return 0;
+
+	if (get_task_state(prev) == TASK_RUNNING)
+		trace_enqueue(BPF_CORE_READ(prev, tgid), BPF_CORE_READ(prev, pid));
+
+	pid = BPF_CORE_READ(next, pid);
+	tsp = bpf_map_lookup_elem(&start, &pid);
+	if (!tsp)
+		return 0;
+	delta = bpf_ktime_get_ns() - *tsp;
+	if (delta < 0)
+		goto cleanup;
+
+	if (target_per_process)
+		hkey = BPF_CORE_READ(next, tgid);
+	else if (target_per_thread)
+		hkey = pid;
+	else if (target_per_pidns)
+		hkey = pid_namespace(next);
+	else
+		hkey = -1;
+
+	histp = bpf_map_lookup_or_try_init(&hists, &hkey, &zero);
+	if (!histp)
+		goto cleanup;
+
+	if (!histp->comm[0])
+		BPF_CORE_READ_STR_INTO(&histp->comm, next, comm);
+	if (target_ms)
+		delta /= 1000000U;
+	else
+		delta /= 1000U;
+
+	slot = log2l(delta);
+	if (slot >= MAX_SLOTS)
+		slot = MAX_SLOTS - 1;
+	__sync_fetch_and_add(&histp->slots[slot], 1);
+
+cleanup:
+	bpf_map_delete_elem(&start, &pid);
+	return 0;
+}
