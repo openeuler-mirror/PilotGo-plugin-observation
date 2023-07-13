8b1441fb49284775896b8354264f98ed453b2778
jianli-97 <lijian2@kylinos.cn>
2023-07-13 09:41:15 +0000

    add print_acq_header and print_acq_task

--- src/klockstat/klockstat.h
+++ src/klockstat/klockstat.h
@@ -2,4 +2,14 @@
 #ifndef __KLOCKSTAT_H
 #define __KLOCKSTAT_H
 
+#define TASK_COMM_LEN		16
+#define PERF_MAX_STACK_DEPTH	127
+
+struct lock_stat {
+	__u64 acq_count;
+	__u64 acq_total_time;
+	__u64 acq_max_time;
+	char acq_max_comm[TASK_COMM_LEN];
+};
+
 #endif
