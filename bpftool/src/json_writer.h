d0357dbea20a5fae667a2e54f024eab3ac926df8
lauk <liukuo@kylinos.cn>
2023-07-18 17:16:40 +0800

    add json printf

--- bpftool/src/json_writer.h
+++ bpftool/src/json_writer.h
@@ -22,3 +22,5 @@ void jsonw_start_array(json_writer_t *self);
 void jsonw_start_object(json_writer_t *self);
 
 void jsonw_name(json_writer_t *self, const char *name);
+
+void __printf(2, 3) jsonw_printf(json_writer_t *self, const char *fmt, ...);
