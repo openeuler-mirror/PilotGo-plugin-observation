4c73fdb8a2cf1a382e26285e5cbbdcd47a70b025
lauk <liukuo@kylinos.cn>
2023-07-28 10:14:36 +0800

    add show and help subcommand

--- bpftool/src/perf.c
+++ bpftool/src/perf.c
@@ -15,6 +15,35 @@
 
 #include "main.h"
 
+static int do_show(int argc, char **argv)
+{
+	int err;
+
+	if (!has_perf_query_support())
+		return -1;
+
+	if (json_output)
+		jsonw_start_array(json_wtr);
+	err = show_proc();
+	if (json_output)
+		jsonw_end_array(json_wtr);
+
+	return err;
+}
+
+static int do_help(int argc, char **argv)
+{
+	fprintf(stderr,
+		"Usage: %1$s %2$s { show | list }\n"
+		"       %1$s %2$s help }\n"
+		"\n"
+		"       " HELP_SPEC_OPTIONS " }\n"
+		"",
+		bin_name, argv[-2]);
+
+	return 0;
+}
+
 static const struct cmd cmds[] = {
 	{ "show",	do_show },
 	{ "list",	do_show },
