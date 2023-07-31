49174c6dd4ec1cf7339c9aef2f4d0000a74eca65
lauk <liukuo@kylinos.cn>
2023-07-31 10:13:28 +0800

    Implement the function of feature subcommand help

--- bpftool/src/feature.c
+++ bpftool/src/feature.c
@@ -1,10 +1,55 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <net/if.h>
+#ifdef USE_LIBCAP
+#include <sys/capability.h>
+#endif
+#include <sys/utsname.h>
+#include <sys/vfs.h>
+
+#include <linux/filter.h>
+#include <linux/limits.h>
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <zlib.h>
+
 #include "main.h"
 
 #ifndef PROC_SUPER_MAGIC
 # define PROC_SUPER_MAGIC	0x9fa0
 #endif
 
+static int do_help(int argc, char **argv)
+{
+	if (json_output) {
+		jsonw_null(json_wtr);
+		return 0;
+	}
 
+	fprintf(stderr,
+		"Usage: %1$s %2$s probe [COMPONENT] [full] [unprivileged] [macros [prefix PREFIX]]\n"
+		"       %1$s %2$s list_builtins GROUP\n"
+		"       %1$s %2$s help\n"
+		"\n"
+		"       COMPONENT := { kernel | dev NAME }\n"
+		"       GROUP := { prog_types | map_types | attach_types | link_types | helpers }\n"
+		"       " HELP_SPEC_OPTIONS " }\n"
+		"",
+		bin_name, argv[-2]);
+
+	return 0;
+}
+
+static const struct cmd cmds[] = {
+	{ "probe",		do_probe },
+	{ "list_builtins",	do_list_builtins },
+	{ "help",		do_help },
+	{ 0 }
+};
 
 int do_feature(int argc, char **argv)
 {
