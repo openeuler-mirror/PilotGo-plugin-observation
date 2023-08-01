79bb15d0a82f9b73653a342f493f387a9a6ffe27
Jackwike <yajun@kylinos.cn>
2023-08-01 09:23:44 +0800

    Defines a constant of prog type

--- libbpf/src/libbpf.c
+++ libbpf/src/libbpf.c
@@ -151,3 +151,58 @@ static const char *const map_type_name[] = {
     [BPF_MAP_TYPE_USER_RINGBUF] = "user_ringbuf",
     [BPF_MAP_TYPE_CGRP_STORAGE] = "cgrp_storage",
 };
+
+static const char *const prog_type_name[] = {
+    [BPF_PROG_TYPE_UNSPEC] = "unspec",
+    [BPF_PROG_TYPE_SOCKET_FILTER] = "socket_filter",
+    [BPF_PROG_TYPE_KPROBE] = "kprobe",
+    [BPF_PROG_TYPE_SCHED_CLS] = "sched_cls",
+    [BPF_PROG_TYPE_SCHED_ACT] = "sched_act",
+    [BPF_PROG_TYPE_TRACEPOINT] = "tracepoint",
+    [BPF_PROG_TYPE_XDP] = "xdp",
+    [BPF_PROG_TYPE_PERF_EVENT] = "perf_event",
+    [BPF_PROG_TYPE_CGROUP_SKB] = "cgroup_skb",
+    [BPF_PROG_TYPE_CGROUP_SOCK] = "cgroup_sock",
+    [BPF_PROG_TYPE_LWT_IN] = "lwt_in",
+    [BPF_PROG_TYPE_LWT_OUT] = "lwt_out",
+    [BPF_PROG_TYPE_LWT_XMIT] = "lwt_xmit",
+    [BPF_PROG_TYPE_SOCK_OPS] = "sock_ops",
+    [BPF_PROG_TYPE_SK_SKB] = "sk_skb",
+    [BPF_PROG_TYPE_CGROUP_DEVICE] = "cgroup_device",
+    [BPF_PROG_TYPE_SK_MSG] = "sk_msg",
+    [BPF_PROG_TYPE_RAW_TRACEPOINT] = "raw_tracepoint",
+    [BPF_PROG_TYPE_CGROUP_SOCK_ADDR] = "cgroup_sock_addr",
+    [BPF_PROG_TYPE_LWT_SEG6LOCAL] = "lwt_seg6local",
+    [BPF_PROG_TYPE_LIRC_MODE2] = "lirc_mode2",
+    [BPF_PROG_TYPE_SK_REUSEPORT] = "sk_reuseport",
+    [BPF_PROG_TYPE_FLOW_DISSECTOR] = "flow_dissector",
+    [BPF_PROG_TYPE_CGROUP_SYSCTL] = "cgroup_sysctl",
+    [BPF_PROG_TYPE_RAW_TRACEPOINT_WRITABLE] = "raw_tracepoint_writable",
+    [BPF_PROG_TYPE_CGROUP_SOCKOPT] = "cgroup_sockopt",
+    [BPF_PROG_TYPE_TRACING] = "tracing",
+    [BPF_PROG_TYPE_STRUCT_OPS] = "struct_ops",
+    [BPF_PROG_TYPE_EXT] = "ext",
+    [BPF_PROG_TYPE_LSM] = "lsm",
+    [BPF_PROG_TYPE_SK_LOOKUP] = "sk_lookup",
+    [BPF_PROG_TYPE_SYSCALL] = "syscall",
+};
+
+static int __base_pr(enum libbpf_print_level level, const char *format,
+                     va_list args)
+{
+    if (level == LIBBPF_DEBUG)
+        return 0;
+
+    return vfprintf(stderr, format, args);
+}
+
+static libbpf_print_fn_t __libbpf_pr = __base_pr;
+
+libbpf_print_fn_t libbpf_set_print(libbpf_print_fn_t fn)
+{
+    libbpf_print_fn_t old_print_fn;
+
+    old_print_fn = __atomic_exchange_n(&__libbpf_pr, fn, __ATOMIC_RELAXED);
+
+    return old_print_fn;
+}
