e7470692b26434a9e4faca947749dd23cfedbc0e
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-08-04 10:28:49 +0800

    define the struct of cred

--- vmlinux/arm64/vmlinux_601.h
+++ vmlinux/arm64/vmlinux_601.h
@@ -1544,3 +1544,44 @@ struct kernel_cap_struct {
 };
 
 typedef struct kernel_cap_struct kernel_cap_t;
+
+struct user_struct;
+
+struct ucounts;
+
+struct group_info;
+
+struct cred {
+	atomic_t usage;
+	atomic_t subscribers;
+	void *put_addr;
+	unsigned int magic;
+	kuid_t uid;
+	kgid_t gid;
+	kuid_t suid;
+	kgid_t sgid;
+	kuid_t euid;
+	kgid_t egid;
+	kuid_t fsuid;
+	kgid_t fsgid;
+	unsigned int securebits;
+	kernel_cap_t cap_inheritable;
+	kernel_cap_t cap_permitted;
+	kernel_cap_t cap_effective;
+	kernel_cap_t cap_bset;
+	kernel_cap_t cap_ambient;
+	unsigned char jit_keyring;
+	struct key *session_keyring;
+	struct key *process_keyring;
+	struct key *thread_keyring;
+	struct key *request_key_auth;
+	void *security;
+	struct user_struct *user;
+	struct user_namespace *user_ns;
+	struct ucounts *ucounts;
+	struct group_info *group_info;
+	union {
+		int non_rcu;
+		struct callback_head rcu;
+	};
+};
