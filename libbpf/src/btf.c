0b7d97814fe4a6c5c8b22a50242ccc6fa7ac36a0
liqiuyu123 <liqiuyu@kylinos.cn>
2023-08-07 15:20:43 +0800

    Add a source BTF type to the target BTF

--- libbpf/src/btf.c
+++ libbpf/src/btf.c
@@ -1547,3 +1547,30 @@ static int btf_rewrite_str(__u32 *str_off, void *ctx)
 	*str_off = off;
 	return 0;
 }
+
+int btf__add_type(struct btf *btf, const struct btf *src_btf, const struct btf_type *src_type)
+{
+	struct btf_pipe p = { .src = src_btf, .dst = btf };
+	struct btf_type *t;
+	int sz, err;
+
+	sz = btf_type_size(src_type);
+	if (sz < 0)
+		return libbpf_err(sz);
+
+	/* deconstruct BTF, if necessary, and invalidate raw_data */
+	if (btf_ensure_modifiable(btf))
+		return libbpf_err(-ENOMEM);
+
+	t = btf_add_type_mem(btf, sz);
+	if (!t)
+		return libbpf_err(-ENOMEM);
+
+	memcpy(t, src_type, sz);
+
+	err = btf_type_visit_str_offs(t, btf_rewrite_str, &p);
+	if (err)
+		return libbpf_err(err);
+
+	return btf_commit_type(btf, sz);
+}
