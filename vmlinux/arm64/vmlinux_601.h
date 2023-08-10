a477f77cff110a4574d8360d01b68d3124b7f363
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-08-10 13:39:15 +0800

    define the struct of key

--- vmlinux/arm64/vmlinux_601.h
+++ vmlinux/arm64/vmlinux_601.h
@@ -2126,3 +2126,49 @@ struct assoc_array {
 	struct assoc_array_ptr *root;
 	long unsigned int nr_leaves_on_tree;
 };
+
+struct key_user;
+
+struct key_restriction;
+
+struct key {
+	refcount_t usage;
+	key_serial_t serial;
+	union {
+		struct list_head graveyard_link;
+		struct rb_node serial_node;
+	};
+	struct rw_semaphore sem;
+	struct key_user *user;
+	void *security;
+	union {
+		time64_t expiry;
+		time64_t revoked_at;
+	};
+	time64_t last_used_at;
+	kuid_t uid;
+	kgid_t gid;
+	key_perm_t perm;
+	short unsigned int quotalen;
+	short unsigned int datalen;
+	short int state;
+	long unsigned int flags;
+	union {
+		struct keyring_index_key index_key;
+		struct {
+			long unsigned int hash;
+			long unsigned int len_desc;
+			struct key_type *type;
+			struct key_tag *domain_tag;
+			char *description;
+		};
+	};
+	union {
+		union key_payload payload;
+		struct {
+			struct list_head name_link;
+			struct assoc_array keys;
+		};
+	};
+	struct key_restriction *restrict_link;
+};
