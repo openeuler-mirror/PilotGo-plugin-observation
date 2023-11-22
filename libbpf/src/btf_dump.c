697ad2ee01029f59ed47372f492eb228753ebe6f
liqiuyu123 <liqiuyu@kylinos.cn>
2023-11-22 16:32:53 +0800

    Provides the data structure and status information required for dump operations of BTF type data

--- libbpf/src/btf_dump.c
+++ libbpf/src/btf_dump.c
@@ -41,3 +41,79 @@ enum btf_dump_type_emit_state {
 	EMITTING,
 	EMITTED,
 };
+
+/* per-type auxiliary state */
+struct btf_dump_type_aux_state {
+	/* topological sorting state */
+	enum btf_dump_type_order_state order_state: 2;
+	/* emitting state used to determine the need for forward declaration */
+	enum btf_dump_type_emit_state emit_state: 2;
+	/* whether forward declaration was already emitted */
+	__u8 fwd_emitted: 1;
+	/* whether unique non-duplicate name was already assigned */
+	__u8 name_resolved: 1;
+	/* whether type is referenced from any other type */
+	__u8 referenced: 1;
+};
+
+/* indent string length; one indent string is added for each indent level */
+#define BTF_DATA_INDENT_STR_LEN			32
+
+/*
+ * Common internal data for BTF type data dump operations.
+ */
+struct btf_dump_data {
+	const void *data_end;		/* end of valid data to show */
+	bool compact;
+	bool skip_names;
+	bool emit_zeroes;
+	__u8 indent_lvl;	/* base indent level */
+	char indent_str[BTF_DATA_INDENT_STR_LEN];
+	/* below are used during iteration */
+	int depth;
+	bool is_array_member;
+	bool is_array_terminated;
+	bool is_array_char;
+};
+
+struct btf_dump {
+	const struct btf *btf;
+	btf_dump_printf_fn_t printf_fn;
+	void *cb_ctx;
+	int ptr_sz;
+	bool strip_mods;
+	bool skip_anon_defs;
+	int last_id;
+
+	/* per-type auxiliary state */
+	struct btf_dump_type_aux_state *type_states;
+	size_t type_states_cap;
+	/* per-type optional cached unique name, must be freed, if present */
+	const char **cached_names;
+	size_t cached_names_cap;
+
+	/* topo-sorted list of dependent type definitions */
+	__u32 *emit_queue;
+	int emit_queue_cap;
+	int emit_queue_cnt;
+
+	/*
+	 * stack of type declarations (e.g., chain of modifiers, arrays,
+	 * funcs, etc)
+	 */
+	__u32 *decl_stack;
+	int decl_stack_cap;
+	int decl_stack_cnt;
+
+	/* maps struct/union/enum name to a number of name occurrences */
+	struct hashmap *type_names;
+	/*
+	 * maps typedef identifiers and enum value names to a number of such
+	 * name occurrences
+	 */
+	struct hashmap *ident_names;
+	/*
+	 * data for typed display; allocated if needed.
+	 */
+	struct btf_dump_data *typed_dump;
+};
