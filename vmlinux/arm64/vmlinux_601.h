049abd296341e12aade2e8c8fa1968093840b9c1
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-11-24 09:10:29 +0800

    define kobject

--- vmlinux/arm64/vmlinux_601.h
+++ vmlinux/arm64/vmlinux_601.h
@@ -2581,3 +2581,18 @@ struct attribute_group {
 struct kset;
 
 struct kobj_type;
+
+struct kobject {
+	const char *name;
+	struct list_head entry;
+	struct kobject *parent;
+	struct kset *kset;
+	const struct kobj_type *ktype;
+	struct kernfs_node *sd;
+	struct kref kref;
+	unsigned int state_initialized: 1;
+	unsigned int state_in_sysfs: 1;
+	unsigned int state_add_uevent_sent: 1;
+	unsigned int state_remove_uevent_sent: 1;
+	unsigned int uevent_suppress: 1;
+};
